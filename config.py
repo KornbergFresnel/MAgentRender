c0c78b533f1925077824ceea041822d439d09568
Andrew Kornberg <kornbergfresnel@outlook.com>
2018-04-20 21:57:05 +0800

    update

--- config.py
+++ config.py
@@ -7,9 +7,13 @@ MAIN_BG_COLOR = (255, 255, 255)
 PLAYER_BG_COLOR = (255, 255, 255)
 CONTROL_BG_COLOR = (0, 42, 56)
 SPLIT_ETA = 0.7  # split main window for player and control panel
+FPS = 60
 
 
 class Color:
     BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
     AGENT = (94, 156, 198)
+    HOVERED = (255, 0, 0)
+    CLICKED = (0, 255, 0)
+    INFO = (76, 114, 176)
