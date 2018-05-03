7f1297dd1a219a8187bdc596154cc0f2b63f19a7
Kornberg <kornbergfresnel@outlook.com>
2018-05-04 01:03:48 +0800

    update with state

--- player/agent.py
+++ player/agent.py
@@ -1,5 +1,6 @@
 from pygame.sprite import Sprite
 from pygame.sprite import Group
+from test.meta import State, StateList
 
 
 class Agent(Sprite):
@@ -12,33 +13,30 @@ class Agent(Sprite):
         self.x_min, self.x_max, self.y_min, self.y_max = \
             x_min, x_max, y_min, y_max
 
-        init = np.random.choice([1, 2, 3, 4], 1)[0]
-        inity = np.random.choice([1, 2, 3, 4], 1)[0]
-        self.dir_x, self.dir_y = np.random.choice([-init, init], 1), np.random.choice([-inity, inity], 1)
-        self.stat = 1
-
-    def update_state(self, state):
-        self.rect.move_ip(state[2][0], state[2][1])
+    def update_state(self, state: State):
+        self.rect.move_ip(*state.position)
 
     def update(self):
         self.rect.move_ip(self.dir_x, self.dir_y)
 
 
 class AgentGroup(Group):
-    def __init__(self, agent_num: int, stats: dict, color: tuple, w: int, h: int, x_min: int, x_max: int, y_min: int, y_max: int):
+    def __init__(self, agent_num: int, stats: StateList, color: tuple, w: int, h: int, x_min: int, x_max: int, y_min: int, y_max: int):
         self.n_agent = agent_num
-        self.inner_agent = {}
-        for i in range(agent_num):
-            self.inner_agent[i] = Agent(i, color, pos, w, h, x_min, x_max, y_min, y_max)
-            self.add(Agent(self.inner_agent[i])
+        self._agent = {}
 
-    def update_states(self, states):
+        for i in range(agent_num):
+            self._agent[stats[i].id] = Agent(stats[i].id, color, stats[i].position, w, h, x_min, x_max, y_min, y_max)
+            self.add(Agent(self.inner_agent[i]))
+
+    def update_states(self, states: StateList):
         """States: id, alive, position
         """
 
         assert len(states) == self.n_agent
         for i in range(self.n_agent):
-            if states[i][1] == False:
-                self.remove(self.inner_agent[i])
+            if states[i].alive == False:
+                self.remove(self._agent[states[i].id])
+                self.n_agent -= 1
             else:
-                self.inner_agent[i].update_state(states[i])
+                self._agent[states[i].id].update_state(states[i])
